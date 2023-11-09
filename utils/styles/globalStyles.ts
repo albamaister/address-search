797116d3377f072bb73d7c8ad31b5fcc921b05b7
Bryan Alba <balbasar@pichincha.com>
2023-11-08 21:41:47 -0500

    Clean structure

---
+++ utils/styles/globalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+  html {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color-scheme: light dark;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
+  }
+
+`;
---
+++ utils/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components'
+
+const Theme: DefaultTheme = {
+  breakpoints: {
+    xs: '320px',
+    sm: '576px',
+    md: '768px',
+  }
+}
+
+export default Theme
